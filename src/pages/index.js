d98e310cbb18992e6c586ccdd9e075468288ad8a
Johannes Obermair <johannes.obermair@gmail.com>
2019-12-07 12:06:04 +0100

    Add offline indicator

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,39 +3,46 @@ import Icon from "@mdi/react";
 import React from "react";
 import Image from "../components/image";
 import Layout from "../components/layout";
+import { OfflineHint } from "../components/OfflineHint";
 import SEO from "../components/seo";
+import { useNetwork } from "../utils/useNetwork";
 import Styles from "./index.module.css";
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <Image />
-    <h1>Johannes Obermair</h1>
-    <p>Software developer from Salzburg, Austria.</p>
-    <div className={Styles.social}>
-      <a
-        href="mailto:johannes.obermair@gmail.com"
-        title="Send me an email"
-        className={`${Styles.link} ${Styles.gmail}`}
-      >
-        <Icon path={mdiGmail} size={1.5} />
-      </a>
-      <a
-        href="https://www.twitter.com/johnnyomair"
-        title="Link to my Twitter profile"
-        className={`${Styles.link} ${Styles.twitter}`}
-      >
-        <Icon path={mdiTwitter} size={1.5} />
-      </a>
-      <a
-        href="https://www.github.com/johnnyomair"
-        title="Link to my GitHub profile"
-        className={`${Styles.link} ${Styles.github}`}
-      >
-        <Icon path={mdiGithubCircle} size={1.5} />
-      </a>
-    </div>
-  </Layout>
-);
+const IndexPage = () => {
+  const isOnline = useNetwork();
+
+  return (
+    <Layout>
+      <SEO title="Home" />
+      <Image />
+      <h1>Johannes Obermair</h1>
+      <p>Software developer from Salzburg, Austria.</p>
+      <div className={Styles.social}>
+        <a
+          href="mailto:johannes.obermair@gmail.com"
+          title="Send me an email"
+          className={`${Styles.link} ${Styles.gmail}`}
+        >
+          <Icon path={mdiGmail} size={1.5} />
+        </a>
+        <a
+          href="https://www.twitter.com/johnnyomair"
+          title="Link to my Twitter profile"
+          className={`${Styles.link} ${Styles.twitter}`}
+        >
+          <Icon path={mdiTwitter} size={1.5} />
+        </a>
+        <a
+          href="https://www.github.com/johnnyomair"
+          title="Link to my GitHub profile"
+          className={`${Styles.link} ${Styles.github}`}
+        >
+          <Icon path={mdiGithubCircle} size={1.5} />
+        </a>
+      </div>
+      {!isOnline && <OfflineHint />}
+    </Layout>
+  );
+};
 
 export default IndexPage;
