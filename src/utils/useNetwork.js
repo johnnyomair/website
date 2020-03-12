932faeb309f6d62ed8dec2eaf952266fe9be7139
Johannes Obermair <johannes.obermair@gmail.com>
2020-03-12 19:08:02 +0100

    Revert "Fix window access"
    This reverts commit c0a6a023ed6c3ce6f07e60d35ea048529118c519.

--- src/utils/useNetwork.js
+++ src/utils/useNetwork.js
@@ -1,23 +1,16 @@
 import { useEffect, useState } from "react";
 
 export function useNetwork() {
-  const [isOnline, setNetwork] = useState(
-    typeof window !== `undefined` ? window.navigator.onLine : true,
-  );
+  const [isOnline, setNetwork] = useState(window.navigator.onLine);
   const updateNetwork = () => {
     setNetwork(window.navigator.onLine);
   };
   useEffect(() => {
-    if (typeof window !== `undefined`) {
-      window.addEventListener("offline", updateNetwork);
-      window.addEventListener("online", updateNetwork);
-    }
-
+    window.addEventListener("offline", updateNetwork);
+    window.addEventListener("online", updateNetwork);
     return () => {
-      if (typeof window !== `undefined`) {
-        window.removeEventListener("offline", updateNetwork);
-        window.removeEventListener("online", updateNetwork);
-      }
+      window.removeEventListener("offline", updateNetwork);
+      window.removeEventListener("online", updateNetwork);
     };
   });
   return isOnline;
