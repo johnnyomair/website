41a3b7d1fa3a0607771a0fd4cee96d2b837428b0
Johannes Obermair <johannes.obermair@icloud.com>
2021-12-30 14:57:16 +0100

    Update favicon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,12 +5,16 @@ class MyDocument extends Document {
     return (
       <Html lang="en">
         <Head>
+          {/* https://evilmartians.com/chronicles/how-to-favicon-in-2021-six-files-that-fit-most-needs */}
+          <link rel="manifest" href="/manifest.webmanifest" />
+          <link rel="icon" href="/favicon.ico" sizes="any" />
+          <link rel="icon" href="/icon.svg" type="image/svg+xml" />
+          <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+          <meta name="theme-color" content="#ffffff" />
           <link
             href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500&display=swap"
             rel="stylesheet"
           />
-          <link rel="icon" href="/favicon.ico" />
-          <meta name="theme-color" content="#ffffff" />
         </Head>
         <body>
           <Main />
