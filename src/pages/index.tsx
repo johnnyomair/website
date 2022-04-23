6e14c15ccb881ba496e1e24940ec5e691e4e6366
Johannes Obermair <johannes.obermair@icloud.com>
2022-04-23 15:37:41 +0200

    Add link to Weddember

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,7 +6,7 @@ import portraitPicture from "../../public/portrait-picture.jpg";
 
 const Home: NextPage = () => {
   return (
-    <div className={styles.container}>
+    <>
       <Head>
         <title>Johannes Obermair</title>
         <meta
@@ -15,27 +15,41 @@ const Home: NextPage = () => {
         />
       </Head>
       <main className={styles.main}>
-        <div className={styles.portraitContainer}>
-          <Image
-            src={portraitPicture}
-            alt="Picture of Johannes"
-            width={200}
-            height={200}
-            placeholder="blur"
-          />
-        </div>
+        <header className={styles.header}>
+          <div className={styles.portraitContainer}>
+            <Image
+              src={portraitPicture}
+              alt="Picture of Johannes"
+              width={200}
+              height={200}
+              placeholder="blur"
+            />
+          </div>
 
-        <h1 className={styles.title}>Johannes Obermair</h1>
+          <h1 className={styles.title}>Johannes Obermair</h1>
 
-        <p className={styles.description}>
-          Software developer from Salzburg, Austria.
-        </p>
+          <p className={styles.description}>
+            Software developer from Salzburg, Austria.
+          </p>
 
-        <a href="mailto:mail@johannesobermair.com" className={styles.contact}>
-          Contact
-        </a>
+          <a href="mailto:mail@johannesobermair.com" className={styles.contact}>
+            Contact
+          </a>
+        </header>
+        <section className={styles.projects}>
+          <h2 className={styles.titleProjects}>Currently working on</h2>
+          <a href="https://weddember.com">
+            {/* eslint-disable-next-line @next/next/no-img-element */}
+            <img
+              src="/weddember.svg"
+              alt="Weddember logo"
+              width="200"
+              height="40"
+            />
+          </a>
+        </section>
       </main>
-    </div>
+    </>
   );
 };
 
