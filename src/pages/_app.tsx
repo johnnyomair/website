275ee1aaf25efd87871d237e49624b0642941a08
Johannes Obermair <johannes.obermair@icloud.com>
2022-08-19 19:45:04 +0200

    Use Fontsource for Montserrat font

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,7 @@
-import "../styles/globals.css";
+import "@fontsource/montserrat";
+import "@fontsource/montserrat/500.css";
 import type { AppProps } from "next/app";
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -11,10 +11,6 @@ class MyDocument extends Document {
           <link rel="icon" href="/icon.svg" type="image/svg+xml" />
           <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
           <meta name="theme-color" content="#ffffff" />
-          <link
-            href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500&display=swap"
-            rel="stylesheet"
-          />
         </Head>
         <body>
           <Main />
